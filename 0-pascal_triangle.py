f45ed21bcc8db1d785e8d728e02e5372b9615d27
Badr-Annabi <badrannabi@gmail.com>
2024-06-02 16:33:17 +0100

    Add triangle

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -2,11 +2,13 @@
 def pascal_triangle(n):
     if n <= 0:
         return
+    triangle = []
     current_row = [1]
     for i in range(n):
-        print(current_row)
+        triangle.append(current_row)
         next_row = [1]
         for j in range(1, len(current_row)):
             next_row.append(current_row[j-1] + current_row[j])
         next_row.append(1)
         current_row = next_row
+    return triangle
