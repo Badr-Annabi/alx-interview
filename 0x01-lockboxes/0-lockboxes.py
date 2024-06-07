ea5b4e49197e5d5d49413cc7dd3309ecc5d48f15
Badr Annabi <badrannabi@Badrs-MacBook-Pro.local>
2024-06-07 01:41:41 +0100

    Test

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-"""This function determines if all the boxes can be opened."""
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially
+from 0 to n - 1 and each box may
+contain keys to the other boxes.
+"""
 
 
 def canUnlockAll(boxes):
@@ -9,21 +14,14 @@ def canUnlockAll(boxes):
     :param boxes:
     :return: True or False
     """
-    if not boxes or type(
-        boxes) is not list or not all(
-            isinstance(box, list) for box in boxes):
+    if not boxes or type(boxes) is not list:
         return False
-    n = len(boxes)
-    visited = set()
-    queue = [0]
 
-    while queue:
-        current = queue.pop(0)
-        if current not in visited:
-            visited.add(current)
-            for key in boxes[current]:
-                if key < n:
-                    queue.append(key)
-    if len(visited) == n:
+    unlocked = [0]
+    for n in unlocked:
+        for key in boxes[n]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
         return True
     return False
