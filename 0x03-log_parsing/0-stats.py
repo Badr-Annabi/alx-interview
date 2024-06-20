82771e1a1555c973b1ca7b314a17685239efce52
Badr_Annabi <badrannabi@gmail.com>
2024-06-20 20:21:02 +0100

    TEST 2

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python3
-""" This Python script reads stdin line by line and computes metrics"""
-"""<IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size>"""
 import sys
 import re
 import signal
 from collections import defaultdict
 from typing import Tuple, Optional
 
+""" This Python script reads stdin line by line and computes metrics"""
+"""<IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
+<status code> <file size>"""
+
 
 def line_parser(line: str) -> Tuple[Optional[int], Optional[int]]:
     """This function Parses each line"""
-    pattern = r'^([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) - \[(\d{2}/\d{2}/\d{4})\] "GET /projects/260 HTTP/1\.1 (\d+) (\d+)"$'
-    match = re.match(pattern, line)
-    if  match:
+    ip = r'([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)'
+    date = r'(\d{2}/\d{2}/\d{4})'
+    req = r'"GET /projects/260 HTTP/1\.1"'
+    stat = r'(\d+)'
+    file = r'(\d+)'
+    pattern = rf'^ {ip} - \[{date}\] {req} {stat} {file}"$'
+    compiled_pattern = re.compile(pattern)
+    match = compiled_pattern.match(line)
+    if match:
         file_size = int(match.group(4))
         status_code = int(match.group(3))
         return file_size, status_code
     else:
         return None, None
+
 
 def print_stats(total_size: int, status_count: defaultdict) -> None:
     """This function prints the current stats"""
@@ -26,28 +34,30 @@ def print_stats(total_size: int, status_count: defaultdict) -> None:
     for code in sorted(status_count):
         print(f"{code}: {status_count[code]}")
 
+
 def main() -> None:
     total_size: int = 0
     status_counts: defaultdict = defaultdict(int)
     line_count: int = 0
-    
+
     try:
         for line in sys.stdin:
             line_count += 1
             file_size, status_code = line_parser(line.strip())
-            
+
             if file_size is None or status_code is None:
                 continue
-            
+
             total_size += file_size
             status_counts[status_code] += 1
-            
+
             if line_count % 10 == 0:
                 print_stats(total_size, status_counts)
-    
+
     except KeyboardInterrupt:
         print_stats(total_size, status_counts)
         sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
