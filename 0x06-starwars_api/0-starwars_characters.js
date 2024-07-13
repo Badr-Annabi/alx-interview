c5b38362b9d120f408473418a010bc7897a399c0
Badr_Annabi <badrannabi@gmail.com>
2024-07-13 03:21:53 +0100

    TASK 0 semistandard

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,15 +6,15 @@ const film = process.argv[2] + '/';
 const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 
 request(filmURL + film, async function (err, res, body) {
-    if (err) return console.error(err);
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-        await new Promise(function (resolve, reject) {
-            request(character, (err, res, body) => {
-                if (err) return console.error(err);
-                console.log(JSON.parse(body).name);
-                resolve();
-            });
-        });
-    }
+  if (err) return console.error(err);
+  const characters = JSON.parse(body).characters;
+  for (const character of characters) {
+    await new Promise(function (resolve, reject) {
+      request(character, (err, res, body) => {
+        if (err) return console.error(err);
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
