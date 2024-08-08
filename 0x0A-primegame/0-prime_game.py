c747f69d0ab22fa2bb3c78f0f50e19f0749821ba
Badr_Annabi <badrannabi@gmail.com>
2024-08-08 12:38:29 +0100

    Prime game add fail cases

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,46 +6,29 @@ def isWinner(x, nums):
     """Function to check who will win the game"""
     if x is None or nums is None or x == 0 or nums == []:
         return None
-    maria = 0
-    ben = 0
-    for num in nums:
-        rounds = list(range(1, num+1))
-        primes = [n for n in range(2, num + 1) if is_prime(n)]
 
-        if not primes:
-            ben += 1
-            continue
-
-        mariasTurn = True
-
-        while(True):
-            if not primes:
-                if mariasTurn:
-                    ben += 1
-                else:
-                    maria += 1
-                break
-            smallestPrime = primes.pop(0)
-            rounds.remove(smallestPrime)
-
-            rounds = [x for x in rounds if x % smallestPrime != 0]
-
-            mariasTurn = not mariasTurn
-
-    if maria > ben:
-        return "Maria"
-
-    if maria < ben:
-        return "Ben"
-
-    return None
-
-
-def is_prime(n):
-    """Returns True if n is prime, else False"""
-    if n < 2:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+    def is_prime(n):
+        """Returns True if n is prime, else False"""
+        if n < 2:
             return False
-    return True
+        for i in range(2, int(n**0.5) + 1):
+            if n % i == 0:
+                return False
+        return True
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for num in nums:
+        primes = [n for n in range(2, num + 1) if is_prime(n)]
+        if len(primes) % 2 == 0:
+            ben_wins += 1
+        else:
+            maria_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
