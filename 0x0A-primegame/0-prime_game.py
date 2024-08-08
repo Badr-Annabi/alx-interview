49be6db37000c44ca43fb0b50f485288353856bb
Badr_Annabi <badrannabi@gmail.com>
2024-08-08 12:47:32 +0100

    Prime game change isWinner func

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,33 +2,32 @@
 """Module for prime game"""
 
 
+def isPrime(n):
+    """this function Return primes list"""
+    prime = []
+    seg_sieve = [True] * (n + 1)
+    for p in range(2, n + 1):
+        if (seg_sieve[p]):
+            prime.append(p)
+            for i in range(p, n + 1, p):
+                seg_sieve[i] = False
+    return prime
+
+
 def isWinner(x, nums):
-    """Function to check who will win the game"""
+    """This function checks who will win maria or ben"""
     if x is None or nums is None or x == 0 or nums == []:
         return None
-
-    def is_prime(n):
-        """Returns True if n is prime, else False"""
-        if n < 2:
-            return False
-        for i in range(2, int(n**0.5) + 1):
-            if n % i == 0:
-                return False
-        return True
-
-    maria_wins = 0
-    ben_wins = 0
-
-    for num in nums:
-        primes = [n for n in range(2, num + 1) if is_prime(n)]
+    Maria = 0
+    Ben = 0
+    for i in range(x):
+        primes = isPrime(nums[i])
         if len(primes) % 2 == 0:
-            ben_wins += 1
+            Ben += 1
         else:
-            maria_wins += 1
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+            Maria += 1
+    if Maria > Ben:
+        return 'Maria'
+    elif Ben > Maria:
+        return 'Ben'
+    return None
